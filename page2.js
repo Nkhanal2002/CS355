793737635cf9df5f38b13befa41acb0db8296493
Narayan Khanal <narayankhanal435@gmail.com>
2024-01-30 00:08:59 -0500

    update index.html, page1.html, and page2.html files

--- page2.js
+++ page2.js
@@ -2,10 +2,17 @@
 const params = new URLSearchParams(location.search);
 const formInfoContainer = $(".form-values");
 
-formInfoContainer.innerHTML = `
-<p class=para>Name is ${params.get("name")}</p>
-<p class=para>Email is ${params.get("email")}</p>
-<p class=para>Age is ${params.get("age")}</p>
-<p class=para>Gender is ${params.get("gender")}</p>
-
+const value =()=>{
+    if (params.get("name") === "" || params.get("email")=== "" || params.get("age") === "" || params.get("gender") === ""){
+        formInfoContainer.innerHTML = "";
+    }
+    else{
+        formInfoContainer.innerHTML = `
+        <p class=para>Name is ${params.get("name")}</p>
+        <p class=para>Email is ${params.get("email")}</p>
+        <p class=para>Age is ${params.get("age")}</p>
+        <p class=para>Gender is ${params.get("gender")}</p>
 `
+    }
+}
+value();
