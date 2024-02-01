53f36aea5d532895a36237858f96b808bb09ae5f
Narayan Khanal <narayankhanal435@gmail.com>
2024-02-01 17:51:42 -0500

    update page2.js file

--- page2.js
+++ page2.js
@@ -1,17 +1,17 @@
 "use strict"
-const params = new URLSearchParams(location.search);
+const formVals = new URLSearchParams(location.search);
 const formInfoContainer = $(".form-values");
 
 const infoValues = () =>{
-        if (params.get("name") === null || params.get("email") === null || params.get("age") === null || params.get("gender")=== null){
+        if (formVals.get("name") === null || formVals.get("email") === null || formVals.get("age") === null || formVals.get("gender")=== null){
                 formInfoContainer.innerHTML = "";
         }
         else{
-                formInfoContainer.innerHTML+= `
-                <p class=para>Name => ${params.get("name")}</p>
-                <p class=para>Email => ${params.get("email")}</p>
-                <p class=para>Age => ${params.get("age")}</p>
-                <p class=para>Gender => ${params.get("gender")}</p>
+                formInfoContainer.innerHTML= `
+                <p class=para>Name => ${formVals.get("name")}</p>
+                <p class=para>Email => ${formVals.get("email")}</p>
+                <p class=para>Age => ${formVals.get("age")}</p>
+                <p class=para>Gender => ${formVals.get("gender")}</p>
         `
         }
 }
